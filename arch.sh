356903941dfdd60e7b6e7ebe1c56df2f782db4e6
Vincent <vc5@users.noreply.github.com>
2023-09-11 15:43:01 +0800

    fix20230911A

--- arch.sh
+++ arch.sh
@@ -5,8 +5,8 @@ if [ "$TARGETARCH" = "arm" ] ; then ARCH=arm;fi
 
 VER=$1
 BUILD_VER=$2
-echo "https://github.com/hpool-dev/chia-miner/releases/download/${VER}/HPool-Miner-chia-og-v${VER}-${BUILD_VER}-linux.zip "
-wget -q --no-check-certificate https://github.com/hpool-dev/chia-miner/releases/download/${VER}/HPool-Miner-chia-og-v${VER}-${BUILD_VER}-linux.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
+echo "https://github.com/h9-dev/chia-miner/releases/download/v${VER}/HPool-Miner-chia-og-v${VER}-${BUILD_VER}-linux.zip "
+wget -q --no-check-certificate https://github.com/h9-dev/chia-miner/releases/download/${VER}/HPool-Miner-chia-og-v${VER}-${BUILD_VER}-linux.zip -O /tmp/chia-miner.zip && unzip -j /tmp/chia-miner.zip -d /tmp/linux
 ls /tmp/linux -ahl
 mv /tmp/linux/hpool-miner-chia-linux-${ARCH} /opt/hpool-chia-miner
 chmod +x /opt/hpool-chia-miner
